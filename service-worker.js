6c81a368cafcced9b1df6472afca0b10d848d937
jeffersonpeixoto <jeffersonpeixoto23@gmail.com>
2026-04-02 11:12:47 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,9 @@
-const CACHE_NAME = "chamados-engenharia-pwa-v3";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./service-worker.js",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+self.addEventListener("install", event => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
-    )
-  );
-  self.clients.claim();
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
-  );
-});
+self.addEventListener("fetch", () => {});
---
+++ supabase.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = "COLE_SUA_URL_AQUI";
+const SUPABASE_KEY = "COLE_SUA_KEY_AQUI";
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
