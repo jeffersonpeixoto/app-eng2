2cb973e6d059f228381d60627e43faddab0751cc
jeffersonpeixoto <jeffersonpeixoto23@gmail.com>
2026-04-02 11:13:57 -0300

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,3 +1,3 @@
-const SUPABASE_URL = "COLE_SUA_URL_AQUI";
-const SUPABASE_KEY = "COLE_SUA_KEY_AQUI";
+const SUPABASE_URL = "https://bubcilkbujuycpvysico.supabase.co";
+const SUPABASE_KEY = "sb_publishable_9ScFMiMlii1eQo-BxvbOZg_LRXvTloH";
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
